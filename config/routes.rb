189093181e654e192d721bd5d20846e7eac3a2c3
Emilio da Costa <emidac@gmail.com>
2017-10-03 12:00:42 -0400

    Implement Auth

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  resources :users
+  resource :session
+
+  resources :subs do
+    resources :posts, only: [:new, :create, :show, :index]
+  end
+
+  resources :posts, only: [:destroy, :edit, :update]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
